926c90616025e5b5656a85a56fc8fdb59b03a795
Guilherme Carvalho <guibrancopc@gmail.com>
2024-11-08 17:25:51 -0300

    feat: create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_08_170053) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_08_201415) do
   create_table "locations", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,6 +24,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_08_170053) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["location_id"], name: "index_recordings_on_location_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "recordings", "locations"
