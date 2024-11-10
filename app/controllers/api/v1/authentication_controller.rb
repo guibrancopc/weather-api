789a4a25f999c59cf8911ec6a39da6f118cc2c2d
Guilherme Carvalho <guibrancopc@gmail.com>
2024-11-10 12:28:49 -0300

    fix: authentication password validation issue

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,9 +2,8 @@ class Api::V1::AuthenticationController < ApiController
   skip_before_action :authenticate_token!
 
   def create
-    # binding.break
     user = User.find_by(email: params[:user][:email])
-    # @FIX_ME: valid_password is not working - check devise documentation to fix this and go to 21min of the video (3)
+
     if user.valid_password? params[:user][:password]
       render json: { token: JsonWebToken.encode(sub: user.id) }
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,19 @@ class User < ApplicationRecord
       email: email
     }
   end
+
+  def valid_password? input_password
+    password == input_password
+  end
+
+  private
+
+  def password
+    @password ||= BCrypt::Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_digest = @password
+  end
 end
